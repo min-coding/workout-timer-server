b8da78230738ff8f0dd1595ddf52c49d3ff81ca5
Min <85483551+min-coding@users.noreply.github.com>
2023-06-05 22:10:08 +0700

    update userRoutes and index

--- src/routes/authRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { User } from '../entity/User';
-import { Request, Response } from 'express';
-import { AppDataSource } from '../data-source';
-import * as bcrypt from 'bcryptjs';
-import * as express from 'express';
-
-const authRouter = express.Router();
-
-export default authRouter;
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,11 +1,36 @@
 import { User } from "../entity/User";
 import { Request, Response } from 'express';
 import { AppDataSource } from '../data-source';
+import * as passport from 'passport';
 import * as bcrypt from 'bcryptjs'
 import * as express from 'express'
+import { isAuth } from "../utils";
 
 const userRouter = express.Router()
 const userRepo = AppDataSource.getRepository(User)
+
+interface UserSession extends Request {
+  user: User
+  flash:Function
+}
+
+userRouter.post(
+  '/signin',
+  passport.authenticate('local', {
+    successRedirect: '/api/users',
+    failureRedirect: '/api/users',
+    failureFlash: true,
+  })
+);
+
+userRouter.get('/', isAuth, (req: UserSession, res: Response) => {
+  try {
+    res.send(`Congrats ${req.user.username} logins successful`);
+  } catch (error) {
+    res.send(req.flash().error)
+  }
+});
+
 
 userRouter.post('/signup', (async (req: Request, res: Response) => {
   try {
@@ -13,6 +38,7 @@ userRouter.post('/signup', (async (req: Request, res: Response) => {
     const hashedPassword = await bcrypt.hashSync(password);
     const user = userRepo.create({ username, password: hashedPassword, email });
     const savedUser = await userRepo.save(user);
+    console.log(`saved user ${savedUser}`)
     return res.send(savedUser);
   } catch (error) {
     console.log(error);
